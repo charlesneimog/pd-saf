31c14471220a3bfb4db7fad3685b5d43e97f8355
Charles K. Neimog <charlesneimog@outlook.com>
2025-05-03 15:24:00 -0300

    add saf object for `declare -lib saf`

---
+++ Sources/saf.c
@@ -0,0 +1,39 @@
+#include <m_pd.h>
+#include <s_stuff.h>
+#include <m_imp.h>
+
+static t_class *saf_libclass;
+
+typedef struct _saf {
+    t_object x_obj;
+} t_saf;
+
+static void *saf_new(void) {
+    return pd_new(saf_libclass);
+}
+
+void saf_setup(void) {
+    int major, minor, micro;
+    sys_getversion(&major, &minor, &micro);
+    if (major < 0 && minor < 54) {
+        return;
+    }
+    saf_libclass =
+        class_new(gensym("saf"), (t_newmethod)saf_new, 0, sizeof(t_saf), CLASS_NOINLET, 0);
+
+    t_canvas *cnv = canvas_getcurrent();
+    const char *requiredLibs[] = {"pdlua"};
+
+    for (size_t i = 0; i < 1; ++i) {
+        int result = sys_load_lib(cnv, requiredLibs[i]);
+        if (!result) {
+            pd_error(NULL,
+                     "[pd-saf] %s not installed, Gui objects will not work! Please, install pdlua",
+                     requiredLibs[i]);
+        }
+    }
+
+    // add to the search path
+    const char *safpath = saf_libclass->c_externdir->s_name;
+    STUFF->st_searchpath = namelist_append(STUFF->st_searchpath, safpath, 0);
+}
